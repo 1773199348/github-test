3521cfcb1d4bd4e565509ed1064e0a515eed1137
1773199348 <48037760+1773199348@users.noreply.github.com>
2021-07-08 17:18:57 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("我将要把这个代码推送到远程库上");
+        
+        System.out.println("pull test");
+        
     }
 }
